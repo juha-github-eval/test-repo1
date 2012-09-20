538b7669aa8ba801c612d71ac5c8be196de4f322
Juha <test@test.com>
2012-09-20 21:44:56 +0300

    Fixed PaulaBean - compiles without warnings my own brain-tissue based compiler and passes all tests (that have been done)

--- src/PaulaBean.cc
+++ src/PaulaBean.cc
@@ -3,10 +3,23 @@
 class PaulaBean
 {
 protected:
-   std::string m_name;
+    std::string m_name;
 public:
-   PaulaBean(const std::string& name = "Brillant");
-   const std::string get_name() const;
+    PaulaBean(const std::string& name = "Brillant");
+    virtual PaulaBean();
+    
+    const std::string get_name() const;
 };
 
+PaulaBean::PaulaBean(const std::string& n)
+    : m_name(name)
+{}
 
+PaulaBean::~PaulaBean()
+{}
+
+const std::string&
+PaulaBean::get_name() const
+{
+    return m_name;
+}
